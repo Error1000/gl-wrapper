dfc272c6cb7498ee0e33745e7121679d5347b6a0
Error1000 <udreadanmihai@gmail.com>
2020-02-14 21:43:01 +0200

    test

--- src/render/texture.rs
+++ src/render/texture.rs
@@ -43,6 +43,7 @@ impl TextureBase {
 pub trait TextureFunc {
     fn bind_texture(self: &Self, sampler_id: GLuint) {
         unsafe {
+            gl::ActiveTexture(gl::TEXTURE0);
             gl::BindTexture(Self::get_type(), self.get_tex_base().id);
             gl::ActiveTexture(gl::TEXTURE0 + sampler_id);
         }
